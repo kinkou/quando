1615f6cf43ae7673d0590eded530ed0a1542e7d1
Sergey Konotopov <lalalalalala@gmail.com>
2019-04-10 21:10:10 +0300

    Optimize defaults and rewrite specs

--- lib/quando/config.rb
+++ lib/quando/config.rb
@@ -16,24 +16,24 @@ module Quando
     def initialize
       @century = 20
 
-      @dlm = /[ -.\/\\]+/
+      @dlm = /[ -.\/]+/
       @year = /(?<year> \d{4})/x
       @year2 = /(?<year> \d{2})/x
       @month_num = /(?<month> 1[0-2] | 0?[1-9])/x
       @day = /(?<day> 3[0-1] | [12][0-9] | 0?[1-9])/x
 
-      @jan = /JANUARY | JAN/ix
-      @feb = /FEBRUARY | FEB/ix
-      @mar = /MARCH | MAR/ix
-      @apr = /APRIL | APR/ix
-      @may = /MAY/ix
-      @jun = /JUNE | JUN/ix
-      @jul = /JULY | JUL/ix
-      @aug = /AUGUST | AUG/ix
-      @sep = /SEPTEMBER | SEPT?/ix
-      @oct = /OCTOBER | OCT/ix
-      @nov = /NOVEMBER | NOV/ix
-      @dec = /DECEMBER | DEC/ix
+      @jan = /JAN(?:UARY)?/xi
+      @feb = /FEB(?:RUARY)? /xi
+      @mar = /MAR(?:CH)?/xi
+      @apr = /APR(?:IL)?/xi
+      @may = /MAY/xi
+      @jun = /JUNE?/xi
+      @jul = /JULY?/xi
+      @aug = /AUG(?:UST)?/xi
+      @sep = /SEP(?:TEMBER)?/xi
+      @oct = /OCT(?:OBER)?/xi
+      @nov = /NOV(?:EMBER)? /xi
+      @dec = /DEC(?:EMBER)? /xi
 
       uniupdate!
     end
@@ -48,7 +48,7 @@ module Quando
     def uniformats!
       @formats = [
         # Formats with a 4-digits year
-        # 14.4.1965, 14/04/1965, 14-4-1965, 14 04 1965, 14\04\1965, …
+        # 14.4.1965, 14/04/1965, 14-4-1965, 14 04 1965, …
         /\A\s* #{@day} #{@dlm} #{@month_num} #{@dlm} #{@year} \s*\z/xi,
 
         # 14-APRIL-1965, 14-apr-1965, 14/Apr/1965, …
@@ -60,12 +60,6 @@ module Quando
         # Same formats with a 2-digits year
         # 13.12.05, 13/12/05, 13-12-05, …
         /\A\s* #{@day} #{@dlm} #{@month_num} #{@dlm} #{@year2} \s*\z/xi,
-
-        # 13-DECEMBER-05, 13-dec-05, …
-        /\A\s* #{@day} #{@dlm} #{@month_txt} #{@dlm} #{@year2} \s*\z/xi,
-
-        # Dec 05, dec.05, DEC-05, …
-        /\A\s* #{@month_txt} #{@dlm} #{@year2} \s*\z/xi,
 
         # April, DECEMBER, sep., …
         /\A\s* #{@month_txt} \s*\z/xi,
--- spec/quando_spec.rb
+++ spec/quando_spec.rb
@@ -1,7 +1,16 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
+require 'securerandom'
+
 RSpec.describe Quando do
+  let(:matz_bday) { Date.new(1965, 4, 14) }
+  let(:matz_bday_txt_rus) { '14 АПРЕЛЯ 1965 г.' }
+  let(:april_rus_rx) { /АПРЕЛ[ЬЯ]/i }
+  let(:year_rus_rx) { /(?<year> \d{4}) \s* (?:Г\.?)?/xi }
+  let(:rails_release) { Date.new(2005, 12, 13) }
+  let(:singe_digit_date) { Date.new(2004, 3, 2) }
+  let(:nonsense) { 'Båtar!' }
 
   describe 'Default day matcher regexp' do
     let(:matcher) { Quando.config.day }
@@ -59,70 +68,124 @@ RSpec.describe Quando do
     end
   end
 
-  let(:matz_bday) { Date.new(1965, 4, 14) }
-  let(:matz_bday_txt_rus) { '14 АПРЕЛЬ 1965' }
-  let(:rails_release) { Date.new(2005, 12, 13) }
-  let(:singe_digit_date) { Date.new(2004, 3, 2) }
-  let(:nonsense) { 'Båtar!' }
+  describe 'Default month name matcher regexps' do
+    let(:matcher) {  }
+  end
+
+  describe 'Configuration' do
+    context 'Class-level' do
+      after { Quando.reset! }
+
+      let(:default_jan_matcher) { Quando.config.jan.source }
+      let(:new_matcher) { SecureRandom.hex(3) }
+
+      it 'Allows set options' do
+        expect(default_jan_matcher).not_to eq(new_matcher)
+
+        Quando.configure do |c|
+          c.jan = new_matcher
+        end
+
+        expect(Quando.config.jan).to eq(new_matcher)
+      end
+
+      def month_txt_updated?
+        Quando.config.month_txt.source.include?(new_matcher)
+      end
+
+      def formats_updated?
+        formats_src = Quando.config.formats.map { |f| f.source }
+        formats_src.any? { |r| r.include?(new_matcher) }
+      end
+
+      it 'Autoupdates compound matchers when part matchers are set' do
+        expect(month_txt_updated?).to eq(false)
+        expect(formats_updated?).to eq(false)
+
+        # c.<feb|sep|etc> = new_matcher
+        random_month = "#{Quando::Config::MONTHS.sample}=".to_sym
+        Quando.configure do |c|
+          c.send(random_month, new_matcher)
+        end
+
+        expect(month_txt_updated?).to eq(true)
+        expect(formats_updated?).to eq(true)
+      end
+
+      it 'Actually works' do
+        expect(Quando.parse matz_bday_txt_rus).to be_nil
+
+        Quando.configure do |c|
+          c.year = year_rus_rx
+          c.apr = april_rus_rx
+        end
+
+        expect(Quando.parse matz_bday_txt_rus).to eq(matz_bday)
+      end
+    end
+
+    context 'Instance-level' do
+      it 'Works like class-level but can be configured independently' do
+        expect(Quando.parse matz_bday_txt_rus).to be_nil
+
+        parser = Quando::Parser.new.configure do |c|
+          c.year = year_rus_rx
+          c.apr = april_rus_rx
+        end
+
+        expect(parser.parse matz_bday_txt_rus).to eq(matz_bday)
+        expect(Quando.parse matz_bday_txt_rus).to be_nil
+      end
+    end
+  end
+
+  describe 'Default configuration' do
+    context 'When input is unrecognizable' do
+      let(:gibberish) do
+        [
+          nil,
+          '2 3 4',
+          '32 3 04',
+          '2 13 04',
+          '14 -=*=- 1965',
+          nonsense,
+        ]
+      end
+
+      it 'Returns nil' do
+        gibberish.each { |g| expect(Quando.parse(g)).to be_nil }
+      end
+    end
+
+    context 'But when it is recognizable' do
+      let(:matz_bday_variety) do
+        [
+          '14.04.1965',
+          '14.4.1965',
+          '14-4-1965',
+          '14 04-1965',
+          '14 4 1965',
+          '14/4/1965',
+          '14 04-1965',
+          '14.04/1965',
+          '14 April 1965',
+          '14 april 1965',
+          '14 APRIL 1965',
+          '14 Apr 1965',
+          '14 Apr. 1965',
+        ]
+      end
+
+      it 'Can parse Matz birthday date in many forms (see inside)' do
+        matz_bday_variety.each { |mbd| expect(Quando.parse(mbd)).to eq(matz_bday) }
+      end
+    end
+  end
 
   specify 'Default date formats' do
-    expect(Quando.parse '14.04.1965').to eq(matz_bday)
     expect(Quando.parse '13.12.05').to eq(rails_release)
     expect(Quando.parse '2-3-04').to eq(singe_digit_date)
-    expect(Quando.parse '14 April 1965').to eq(matz_bday)
-    expect(Quando.parse '14 Apr. 1965').to eq(matz_bday)
-    expect(Quando.parse '14 Apr 1965').to eq(matz_bday)
-    expect(Quando.parse 'Dec 05').to eq(Date.new(2005, 12, 1))
     expect(Quando.parse 'Dec').to eq(Date.new(Time.now.getlocal.year, 12, 1))
-  end
-
-  specify 'Default delimiters' do
-    expect(Quando.parse '14 04-1965').to eq(matz_bday)
-    expect(Quando.parse '14.04/1965').to eq(matz_bday)
-    expect(Quando.parse '14\04 1965').to eq(matz_bday)
-  end
-
-  specify 'Default text month names' do
-    expect(Quando.parse '14 April 1965').to eq(matz_bday)
-    expect(Quando.parse '14 april 1965').to eq(matz_bday)
-    expect(Quando.parse '14 APRIL 1965').to eq(matz_bday)
-    expect(Quando.parse '14 Apr 1965').to eq(matz_bday)
-    expect(Quando.parse '14 Apr. 1965').to eq(matz_bday)
-  end
-
-  specify 'Invalid dates' do
-    expect(Quando.parse nil).to be_nil
-    expect(Quando.parse '2 3 4').to be_nil
-    expect(Quando.parse '32 3 04').to be_nil
-    expect(Quando.parse '2 13 04').to be_nil
-    expect(Quando.parse '14 -=*=- 1965').to be_nil
-    expect(Quando.parse nonsense).to be_nil
-  end
-
-  let(:april_rus_rx) { /АПРЕЛЬ/i }
-
-  specify 'Simple app-level configuration' do
-    expect(Quando.parse matz_bday_txt_rus).to be_nil
-
-    Quando.configure do |c|
-      c.apr = april_rus_rx
-      c.uniupdate!
-    end
-
-    expect(Quando.parse matz_bday_txt_rus).to eq(matz_bday)
-    Quando.reset!
-  end
-
-  specify 'Simple instance-level configuration' do
-    expect(Quando.parse matz_bday_txt_rus).to be_nil
-
-    parser = Quando::Parser.new.configure do |c|
-      c.apr = april_rus_rx
-      c.uniupdate!
-    end
-
-    expect(parser.parse matz_bday_txt_rus).to eq(matz_bday)
-    expect(Quando.parse matz_bday_txt_rus).to be_nil
   end
 
   specify 'Simple one-time configuration' do
